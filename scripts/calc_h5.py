f89aa29017d2e5ea7bdcb0a5009fd7bcca7ba005
Igor Machado <igor.machado@gmail.com>
2025-09-06 15:04:40 -0300

    update script

--- scripts/calc_h5.py
+++ scripts/calc_h5.py
@@ -17,11 +17,18 @@ arquivos = ['SBPO_2019_2019_PPCR1_2025_09.csv',
             'SBPO_2020_2020_PPCR1_2025_09.csv', 
             'SBPO_2021_2021_PPCR1_2025_09.csv', 
             'SBPO_2022_2022_PPCR1_2025_09.csv', 
-            'SBPO_2023_2023_PPCR1_2025_09.csv']
+            'SBPO_2023_2023_PPCR1_2025_09.csv',
+            'SBPO_2024_2024_PPCR1_2025_09.csv']
+
+ANO_REF = 2024
+ANO_INICIO = ANO_REF-5
+ANO_FIM = ANO_REF-1
 
 # dataframe final agregado para o H5
 dfs = []
 
+print(f"Executando script calc_h5.py")
+print(f"Ano Referencia = {ANO_REF}, Ano Inicio = {ANO_INICIO}, Ano Fim = {ANO_FIM}")
 print("=== H5 por ano ===")
 for arq in arquivos:
     caminho = DATA_DIR + arq
@@ -31,9 +38,12 @@ for arq in arquivos:
     linhas_filtradas = []
     for _, row in df_temp.iterrows():
         ano = int(row["Year"])
-        if ano >= 2019 and ano <= 2024:
+        if ano >= ANO_INICIO and ano <= ANO_FIM:
             linhas_filtradas.append(row)
     
+    if len(linhas_filtradas) == 0:
+        print(f"{arq} -> H5 = {0}, Citacoes = {0}, Artigos = {0} [ignorado]...")
+        continue
     df_ano = pd.DataFrame(linhas_filtradas)
     dfs.append(df_ano) 
 
@@ -48,7 +58,7 @@ for arq in arquivos:
     
     h5_ano = calcular_h_index(citacoes)
     num_papers = len(df_ano)
-    print(f"{arq} -> H5 = {h5_ano}, Total de citações = {total_citacoes}, # Artigos = {num_papers}")
+    print(f"{arq} -> H5 = {h5_ano}, Citacoes = {total_citacoes}, Artigos = {num_papers}")
 
 
 df = pd.concat(dfs, ignore_index=True)
@@ -66,6 +76,6 @@ for valor in df["Cites"].fillna(0):
 h5_total = calcular_h_index(citacoes)
 num_papers_total = len(df)
 print("=== H5 Consolidado ===")
-print(f"H5 = {h5_total}, Total de citações = {total_citacoes}, Papers = {num_papers_total}")
+print(f"H5 = {h5_total}, Total Citacoes = {total_citacoes}, Total Artigos = {num_papers_total}")
 
 
