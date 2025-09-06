dd111582157d2eda97d263f72be368b4176fa852
Igor Machado <igor.machado@gmail.com>
2025-09-05 21:24:52 -0300

    testing out .csv format

--- scripts/calc_h5.py
+++ scripts/calc_h5.py
@@ -13,7 +13,7 @@ def calcular_h_index(citacoes):
 
 #
 DATA_DIR = '../data/SBPO/'
-arquivos = ['SBPO_2020_FIX_2025_09_05.csv', 'SBPO_2021_FIX_2025_09_05.csv', 'SBPO_2022_FIX_2025_09_05.csv', 'SBPO_2023_FIX_2025_09_05.csv', 'SBPO_2024_FIX_2025_09_05.csv' ]
+arquivos = ['SBPO_2019_FIX_2025_09_05.csv', 'SBPO_2020_FIX_2025_09_05.csv', 'SBPO_2021_FIX_2025_09_05.csv', 'SBPO_2022_FIX_2025_09_05.csv', 'SBPO_2023_FIX_2025_09_05.csv']
 
 # dataframe final agregado para o H5
 dfs = []
@@ -27,33 +27,41 @@ for arq in arquivos:
     linhas_filtradas = []
     for _, row in df_temp.iterrows():
         ano = int(row["Year"])
-        if ano >= 2020 and ano <= 2024:
+        if ano >= 2019 and ano <= 2024:
             linhas_filtradas.append(row)
     
     df_ano = pd.DataFrame(linhas_filtradas)
     dfs.append(df_ano) 
 
+    total_citacoes = 0
     citacoes = []
     for valor in df_ano["Cites"].fillna(0):
         v = int(valor)
         # minimo de 1 citação para contribuir no fator H
         if v > 0:     
             citacoes.append(v)
+            total_citacoes += v
     
     h5_ano = calcular_h_index(citacoes)
-    print(f"{arq} -> H5 = {h5_ano}")
+    num_papers = len(df_ano)
+    print(f"{arq} -> H5 = {h5_ano}, Total de citações = {total_citacoes}, # Artigos = {num_papers}")
+
 
 df = pd.concat(dfs, ignore_index=True)
 
 # TODO(igormcoelho): fazer uma função pra isso
+total_citacoes = 0
 citacoes = []
 for valor in df["Cites"].fillna(0):
     v = int(valor)
     # minimo de 1 citação para contribuir no fator H
     if v > 0:     
         citacoes.append(v)
+        total_citacoes += v
 
 h5_total = calcular_h_index(citacoes)
+num_papers_total = len(df)
 print("=== H5 Consolidado ===")
-print(f"H5 = {h5_total}")
+print(f"H5 = {h5_total}, Total de citações = {total_citacoes}, Papers = {num_papers_total}")
+
 
